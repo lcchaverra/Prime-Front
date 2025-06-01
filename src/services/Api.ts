0a3a70ff7e8b0cb05e2637d7f68e55cd5ac6774c
lcchaverra <lucas.cordoba1510@gmail.com>
2025-06-01 03:13:27 -0500

    update: se actualiza el manejo de errores del servicio general de la api

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,4 +1,5 @@
 import { type ServiceResponse } from "../types/app";
+import { type ApiError } from "../interface/Api";
 
 export const apiFetch = async (endpoint: string, method = 'GET', body = null, customHeaders = {}) => {
     const token = localStorage.getItem('token');
@@ -9,7 +10,12 @@ export const apiFetch = async (endpoint: string, method = 'GET', body = null, cu
 
         if (!response.ok) {
             const errorData = await response.json().catch(() => null);
-            throw new Error(`Error: ${errorData?.message || response.status}`);
+            const apiError: ApiError = {
+                message: errorData?.message || `Error ${response.status}`,
+                status: errorData?.status || "error",
+                statusCode: response.status
+            };
+            throw apiError;
         }
         const data: ServiceResponse<any> = await response.json();
         return data;
