87426ff3cfbd8347d811ff1f60f13b1cf3376727
lcchaverra <lucas.cordoba1510@gmail.com>
2025-06-01 03:14:25 -0500

    fix: se corrige tipado del hook useToast

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 
 export const useToast = () => {
     const toast = useRef(null);
-    const showToast = (severity, summary, detail, life = 3000) => {
+    const showToast = (severity:string, summary:string, detail:any, life = 3000) => {
       toast.current?.show({ severity, summary, detail, life });
     };
     return { toast, showToast };
